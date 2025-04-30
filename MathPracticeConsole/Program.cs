60f4ad8044ab4b2e46db1ab985769fa24a15a929
Matricarin <matricarin@gmail.com>
2025-04-30 05:04:56 +0500

    loops

--- MathPracticeConsole/Program.cs
+++ MathPracticeConsole/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Matrices;
+
+namespace MathPracticeConsole
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var rows = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            
+            var matrix = new double[rows][];
+            
+            for (int i = 0; i < rows; i++)
+            {
+                matrix[i] = Console.ReadLine()?.Split().Select(double.Parse).ToArray() 
+                            ?? throw new InvalidOperationException();
+            }
+            
+            var adjMatrix = new AdjacencyMatrix(matrix);
+            
+            var loops = adjMatrix.FindLoopsInMatrix();
+            
+            Console.WriteLine(loops.Count == 0 ? "NO LOOPS" : string.Join('\n', loops));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Matrices/AdjacencyMatrix.cs
@@ -0,0 +1,65 @@
+﻿namespace Matrices;
+
+public sealed class AdjacencyMatrix
+{
+    private readonly double[][] _matrix;
+
+    public AdjacencyMatrix(double[][] matrix)
+    {
+        if (!IsSquareMatrix(matrix))
+        {
+            throw new ArgumentException("The matrix must be square.");
+        }
+        _matrix = matrix;
+    }
+
+    public AdjacencyMatrix(int rows, int columns)
+    {
+        if (rows == columns)
+        {
+            throw new ArgumentException("The matrix must be square.");
+        }
+        
+        _matrix = new double[rows][];
+        
+        for (int i = 0; i < rows; i++)
+        {
+            _matrix[i] = new double[columns];
+        }
+    }
+
+    public List<int> FindLoopsInMatrix()
+    {
+        var loops = new List<int>();
+        var rows = _matrix.Length;
+        for (int i = 0; i < rows; i++)
+        {
+            var columns = _matrix[i].Length;
+            for (int j = 0; j < columns; j++)
+            {
+                if (i == j)
+                {
+                    if (_matrix[i][j] != 0)
+                    {
+                        loops.Add(i);
+                    }
+                }
+            }
+        }
+        return loops;
+    }
+
+    private bool IsSquareMatrix(double[][] matrix)
+    {
+        var rows = matrix.Length;
+        for (int i = 0; i < rows; i++)
+        {
+            var columns = matrix[i].Length;
+            if (rows != columns)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
