d6425f15066a41ca477191909039216403497a9b
Matricarin <matricarin@gmail.com>
2025-05-30 06:51:47 +0500

    new problem solved

---
+++ Algorithms/Basic/MathBasic.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.Basic;
+
+public static class MathBasic
+{
+    public static List<int> FindPoint(int px, int py,int qx, int qy)
+    {
+        if (px == qx && py == qy)
+        {
+            return [px, py];
+        }
+
+        var dir = (qx - px, qy - py);
+        var length = Math.Sqrt((qx - px) * (qx - px) + (qy - py) * (qy - py));
+        var targetLength = length * 2;
+        var normal = (dir.Item1 / length, dir.Item2 / length);
+        var target = (px + normal.Item1 * targetLength, py + normal.Item2 * targetLength);
+        
+        return [(int)target.Item1, (int)target.Item2];
+    }
+}
---
+++ AlgorithmsTests/MathBasicFixture.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Basic;
+
+namespace AlgorithmsTests;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class MathBasicFixture
+{
+    [Test]
+    [TestCase(8,4,3,2,-2,0)]
+    public void FindPointTest(int px, int py, int qx, int qy, int x, int y)
+    {
+        var result = MathBasic.FindPoint(px, py, qx, qy);
+        Assert.Multiple(() =>
+        {
+            var resultX = result.First();
+            var resultY = result.Last();
+            Assert.That(resultX, Is.EqualTo(x));
+            Assert.That(resultY, Is.EqualTo(y));
+        });
+    }
+}
