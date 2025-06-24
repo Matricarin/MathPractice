8c0c789668210253d682adbfd39c17ad583b8834
Matricarin <matricarin@gmail.com>
2025-06-24 20:52:18 +0500

    new problem was solved

--- Algorithms/Basic/EasyBasic.cs
+++ Algorithms/Basic/EasyBasic.cs
@@ -2,9 +2,9 @@
 
 namespace Algorithms.Basic;
 
-public class EasyBasic
+public static class EasyBasic
 {
-    public string AddBinary(string a, string b)
+    public static string AddBinary(string a, string b)
     {
         var i = a.Length - 1;
         var j = b.Length - 1;
@@ -30,5 +30,44 @@ public class EasyBasic
         }
 
         return result.ToString();
+    }
+
+    public static List<int> ZigZagSequence(List<int> input)
+    {
+        var ordered = input.OrderBy(i => i).ToList();
+        var first = ordered.Take(ordered.Count  / 2);
+        var second = ordered.GetRange(ordered.Count / 2, ordered.Count - (ordered.Count - 2));
+        var union = first.Union(second).ToList();
+        return union;
+    }
+    
+    public static int FindJudge(int n, int[][] trust) 
+    {
+        var relations = new Relation[n];
+        foreach (var relation in trust)
+        {
+            relations[relation[0] - 1].Out++;
+            relations[relation[1] - 1].In++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (relations[i].Out == 0 && relations[i].In == n - 1)
+            {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private struct Relation
+    {
+        public int In;
+        public int Out;
+        public Relation()
+        {
+            In = 0;
+            Out = 0;
+        }
     }
 }
---
+++ AlgorithmsTests/FindJudgeTest.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.Basic;
+
+namespace AlgorithmsTests;
+
+[TestFixture]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class FindJudgeTest
+{
+    [TestCaseSource(typeof(FindJudgeData), nameof(FindJudgeData.Data))]
+    public void IsPathExistTest(int n, int[][] trust, int expected)
+    {
+        var result = EasyBasic.FindJudge(n, trust);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
+
+public class FindJudgeData
+{
+    public static object[] Data = new object[]
+    {
+        new object[]
+        {
+            2,
+            new int[][]
+            {
+                [1, 2]
+            },
+            2
+        }
+    };
+}
